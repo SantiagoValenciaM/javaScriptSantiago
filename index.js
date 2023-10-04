5dc9986e8c7d51876e11e6fbd3308125dabffb67
SantiagoValenciaM <svalencia7@ucol.mx>
2023-10-04 17:14:52 -0600

    Updated

---
+++ index.js
@@ -0,0 +1,20 @@
+// Hola
+// Declaracion de variables en JS Moderno
+// let contador = 0;
+// contador = 8;
+// const constante = 4;
+
+
+// Alerta en JS
+// alert("Esto es una alerta de JavaScript");
+        // console.log("Hola mundo, esto es javascript desde un archivo html");
+        
+// Concatenacion
+let nombre = 'Santiago';
+let apellido = 'Valencia';
+console.log(nombre + ' ' + apellido);
+
+// document.write('Hola en el documento desde js');
+
+let seleccion = document.querySelector('#seccion1');
+seleccion.innerHTML = `<h3> Mi nombre es ${nombre} </h3>`;
